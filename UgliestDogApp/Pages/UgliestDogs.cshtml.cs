2c97f82949ef0280356d5b378808b00afa26cd02
Taylor McLaughlin <158223645+kalewashere@users.noreply.github.com>
2025-04-08 16:35:06 +0000

    Comments added

--- UgliestDogApp/Pages/UgliestDogs.cshtml.cs
+++ UgliestDogApp/Pages/UgliestDogs.cshtml.cs
@@ -3,18 +3,19 @@ using Microsoft.AspNetCore.Mvc;
    using Microsoft.AspNetCore.Mvc.Rendering;
    using Microsoft.Data.Sqlite;
    using System.Collections.Generic;
-
-   public class UgliestDogsModel : PageModel
+// i canNOT figure out how to make a comment in a .cshtml file, but ill cover it here:
+// in _Layout.cshtml, that code is used to set up the page - from padding and margins to classes (which can further be edited in _Layout.css - my favorite!) and overall structure of the page. 
+   public class UgliestDogsModel : PageModel 
    {
        public List<SelectListItem> DogList { get; set; }
        public Dog SelectedDog { get; set; }
 
-       public void OnGet()
+       public void OnGet() // tells the program to call the loadDogList class and access the list to begin the process of selecting an ugly dog winner
        {
            LoadDogList();
        }
 
-       public void OnPost(string selectedDog)
+       public void OnPost(string selectedDog) 
        {
            LoadDogList();
            if (!string.IsNullOrEmpty(selectedDog))
@@ -23,14 +24,14 @@ using Microsoft.AspNetCore.Mvc;
            }
        }
 
-       private void LoadDogList()
+       private void LoadDogList() // this allows us to load the dog list from the db to access the data
        {
            DogList = new List<SelectListItem>();
            using (var connection = new SqliteConnection("Data Source=UgliestDogs.db"))
            {
                connection.Open();
-               var command = connection.CreateCommand();
-               command.CommandText = "SELECT Id, Name FROM Dogs";
+               var command = connection.CreateCommand(); // sql connection
+               command.CommandText = "SELECT Id, Name FROM Dogs"; // obtains id and name from dogs, run each time the selection is changed
                using (var reader = command.ExecuteReader())
                {
                    while (reader.Read())
@@ -47,7 +48,7 @@ using Microsoft.AspNetCore.Mvc;
 
        private Dog GetDogById(int id)
        {
-           using (var connection = new SqliteConnection("Data Source=UgliestDogs.db"))
+           using (var connection = new SqliteConnection("Data Source=UgliestDogs.db")) // allows us to run and access daa from the DB using sql connection. below, sql commands are set and run each time the dog is changed to update picture and information
            {
                connection.Open();
                var command = connection.CreateCommand();
@@ -72,7 +73,7 @@ using Microsoft.AspNetCore.Mvc;
        }
    }
 
-   public class Dog
+   public class Dog // getters and setters for Dog class (used to pass data back to code)
    {
        public int Id { get; set; }
        public string Name { get; set; }
